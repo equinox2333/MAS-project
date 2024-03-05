689767ab6f98b00f39c861ec6cb26198c1b21e59
roger <roger.xing.success@gmail.com>
2024-03-05 13:10:57 -0500

    initial backend design

---
+++ flaskProject/api/userAPI.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify
+from firebase_admin import firestore
+import uuid
+
+db = firestore.client()
+user_Ref = db.collection('user')
+
+userAPI = Blueprint('userAPI', __name__)
+@userAPI.route('/add',methods=['POST'])
+def create():
+    try:
+        id = uuid.uuid4()
+        user_Ref.document(id.hex).set(request.json)
+        return jsonify({"success":True}),200
+    except Exception as e:
+        return f"Error: {e}",400
+
+# @userAPI.route('/list')
+# def read():
+#     try:
+#         all_users = [doc.to_dict() for doc in user_Ref.stream()]
+#         return jsonify(all_users),200
+#     except Exception as e:
+#         return f"Error: {e}"
