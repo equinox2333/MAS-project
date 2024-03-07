97fc41ae3f0c254b1a48d57abaa0d05ae38f2b15
Zilu Zhu <zhuzilu@Zilus-Mac.attlocal.net>
2024-03-06 22:33:11 -0500

    initial page for creating task

--- frontend/AISplitter/App.js
+++ frontend/AISplitter/App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import CreateTask from './components/temp';
+
+const Stack = createStackNavigator();
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      {/* <StatusBar style='auto' /> */}
+      <Stack.Navigator>
+        <Stack.Screen name="temp" options={{
+          title: 'temporary page',
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },}}
+          component={CreateTask}></Stack.Screen>
+           </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ frontend/AISplitter/components/temp.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+
+const CreateTask = ({navigation}) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const handleSubmit = async () => {
+    try {
+    //   TODO: send to backend
+      console.log("task created");
+    //   navigation.navigate('');
+    } catch (e) {
+      console.error(e);
+    //   alert(e);
+    }
+  };
+
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Task Title</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={setTitle}
+        placeholder="Enter title"
+      />
+      <Text style={styles.label}>Date:</Text>
+      <DateTimePicker
+      testID="dateTimePicker"
+      value={date}
+      mode="date"
+      is24Hour={true}
+      display="default"
+      onChange={onChange}
+      style={styles.date}
+    />
+      <Text style={styles.label}>Content</Text>
+      <TextInput
+        style={[styles.input, styles.textArea]}
+        value={content}
+        onChangeText={setContent}
+        placeholder="Enter content"
+        multiline
+      />
+      <Button title="Creat Task" onPress={handleSubmit} />
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    marginTop: 40,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 100,
+    textAlignVertical: 'top',
+    // borderRadius: '20',
+  },
+  date: {
+    alignSelf: 'left'
+
+  }
+});
+
+export default CreateTask;
