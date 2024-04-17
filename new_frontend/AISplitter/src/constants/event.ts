be925a53fd5e6214da448f5b128948c42124ee4d
Roger Tong Xing <txing001@e.ntu.edu.sg>
2024-04-17 15:16:36 -0400

    use new branch

--- new_frontend/AISplitter/src/constants/event.ts
+++
@@ -1,4 +0,0 @@
-export enum EventKey {
-  /** 本地存储更新 */
-  StoredValueUpdated = 'storedValueUpdated',
-}
--- new_frontend/AISplitter/src/context/ThemeContext.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Theme } from '@/constants/theme';
-
-export const ThemeContext = React.createContext({
-  theme: Theme.Light,
-  toggleTheme: () => {},
-});
