3d2e0fa29e3fce449b0c8ee3cb525778973eec85
Zilu Zhu <zhuzilu@Zilus-Mac.attlocal.net>
2024-04-19 14:19:45 -0400

    fetch

---
+++ flaskProject/functions/main.py
@@ -0,0 +1,16 @@
+# Welcome to Cloud Functions for Firebase for Python!
+# To get started, simply uncomment the below code or create your own.
+# Deploy with `firebase deploy`
+
+from firebase_functions import https_fn
+from firebase_admin import initialize_app
+
+initialize_app()
+
+@app.route('/test')
+def hello_world():
+    return 'Hello, Firebase Cloud Functions with Python'
+
+@https_fn.on_request()
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world!")
